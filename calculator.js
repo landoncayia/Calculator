b462379ee4cca40465645b62f95506caf35c0b09
Landon Cayia <landon.cayia@gmail.com>
2023-04-15 13:24:32 -0400

    Create operate function and variables

--- calculator.js
+++ calculator.js
@@ -2,3 +2,20 @@ const add = (a, b) => (a + b);
 const subtract = (a, b) => (a - b);
 const multiply = (a, b) => (a * b);
 const divide = (a, b) => (b != 0 ? a / b : undefined);
+
+let num1, num2, operator;
+
+const operate = function (operator, num1, num2) {
+  switch(operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+    default:
+      return undefined;
+  }
+};
